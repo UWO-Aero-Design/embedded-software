617fa33b1cf156ab06eea5b0ed13a0104307c9d0
Carl Baron <carbaro196@gmail.com>
2019-10-22 21:02:14 -0400

    Changed submodule reference and fully defined simulated test message

--- western-aero-2019-2020/TestMessages.h
+++ western-aero-2019-2020/TestMessages.h
@@ -32,3 +32,117 @@ aero::def::Pitot_t random_pitot(void) {
     };
     return pitot;
 }
+
+aero::def::GPS_t test_gps(void) {
+    aero::def::GPS_t gps;
+    // int32_t
+    gps.lat = 0xABCDEFAB;
+    // int32_t
+    gps.lon = 0xCDEFABCD;
+    // uint16_t
+    gps.speed = 0xEFAB;
+    // uint8_t
+    gps.satellites = 0xCD;
+    // uint32_t
+    gps.time = 0xEFABCDEF;
+    // uint32_t
+    gps.date = 0xABCDEFAB;
+
+    return gps;
+}
+
+aero::def::Enviro_t test_enviro(void) {
+    aero::def::Enviro_t enviro;
+    // uint16_t for all
+    enviro.pressure = 0xABCD;
+    enviro.humidity = 0xEFAB;
+    enviro.temperature = 0xCDEF;
+
+    return enviro;
+}
+
+aero::def::Battery_t test_battery(void) {
+    aero::def::Battery_t batt;
+    // uint16_t for all
+    batt.voltage = 0xABCD;
+    batt.current = 0xEFAB;
+
+    return batt;
+}
+
+aero::def::SystemConfig_t test_system(void) {
+    aero::def::SystemConfig_t system;
+    // Empty struct
+    return system;
+}
+
+aero::def::Status_t test_status(void) {
+    aero::def::Status_t status;
+    // int16_t
+    status.rssi = 0xABCD;
+    // uint32_t
+    status.state = 0xEFABCDEF;
+
+    return status;
+}
+
+aero::def::Servos_t test_servos(void) {
+    aero::def::Servos_t servos;
+    // All uint32
+    servos.servo0 = 0xABCDEFAB;
+    servos.servo1 = 0xCDEFABCD;
+    servos.servo2 = 0xABCDEFAB;
+    servos.servo3 = 0xCDEFABCD;
+    servos.servo4 = 0xABCDEFAB;
+    servos.servo5 = 0xCDEFABCD;
+    servos.servo6 = 0xABCDEFAB;
+    servos.servo7 = 0xCDEFABCD;
+    servos.servo8 = 0xABCDEFAB;
+    servos.servo9 = 0xCDEFABCD;
+    servos.servo10 = 0xABCDEFAB;
+    servos.servo11 = 0xCDEFABCD;
+    servos.servo12 = 0xABCDEFAB;
+    servos.servo13 = 0xCDEFABCD;
+    servos.servo14 = 0xABCDEFAB;
+    servos.servo15 = 0xCDEFABCD;
+
+    return servos;
+}
+
+aero::def::AirData_t test_airdata(void) {
+    aero::def::AirData_t airdata;
+    // All uint32
+    airdata.ias = 0xABCDEFAB;
+    airdata.eas = 0xCDEFABCD;
+    airdata.tas = 0xABCDEFAB;
+    airdata.agl = 0xCDEFABCD;
+    airdata.pressure_alt = 0xABCDEFAB;
+    airdata.msl = 0xCDEFABCD;
+    airdata.density_alt = 0xABCDEFAB;
+    airdata.approx_temp = 0xCDEFABCD;
+    airdata.density = 0xABCDEFAB;
+    
+    return airdata;
+}
+
+aero::def::Commands_t test_commands(void) {
+    aero::def::Commands_t commands;
+    // uint8_t
+    commands.drop = 0xAB;
+    // uint16_t
+    commands.servos = 0xCDEF;
+    // uint8_t
+    commands.pitch = 0xAB;
+
+    return commands;
+}
+
+aero::def::DropAlgo_t test_dropalgo(void) {
+    aero::def::DropAlgo_t dropalgo;
+    // int16_t
+    dropalgo.heading = 0xABCD;
+    // uint16_t
+    dropalgo.distance = 0xEFAB;
+
+    return dropalgo;
+}
--- western-aero-2019-2020/western-aero-2019-2020.ino
+++ western-aero-2019-2020/western-aero-2019-2020.ino
@@ -5,20 +5,37 @@
 using namespace aero;
 using namespace aero::def;
 
+const bool SEND_IMU = true;
+const bool SEND_PITOT = true;
+const bool SEND_GPS = true;
+const bool SEND_ENV = true;
+const bool SEND_BATT = true;
+const bool SEND_SYSTEM = true;
+const bool SEND_STATUS = true;
+const bool SEND_SERVOS = true;
+const bool SEND_AIRDATA = true;
+const bool SEND_CMDS = true;
+const bool SEND_DROPALGO = true;
+
 Message msg_handler;
 
-
-IMU_t imu;
+IMU_t imu = random_imu();
 Pitot_t pitot = random_pitot();
+GPS_t gps = test_gps();
+Enviro_t enviro = test_enviro();
+Battery_t batt = test_battery();
+SystemConfig_t system_config = test_system();
+Status_t status_state = test_status();
+Servos_t servos = test_servos();
+AirData_t airdata = test_airdata();
+Commands_t commands = test_commands();
+DropAlgo_t dropalgo = test_dropalgo();
 
 void setup() {
-  imu = random_imu();
-  //pitot = 
-  
   // Init serial port
   Serial.begin(115200);
 
-  // Need to seed random number generator from setup()
+  // Need to seed random number generator from setup() if using random
   randomSeed(analogRead(0));
 
   pinMode(led, OUTPUT);
@@ -26,13 +43,29 @@ void setup() {
   
 }
 void loop() {
-  // Generate random data
-  
-  pitot.differential_pressure += 100;
-  // Create message buffer
-  msg_handler.add_imu(imu);
-  
-  msg_handler.add_pitot(pitot);
+  // Add to message buffer
+  if(SEND_IMU) 
+    msg_handler.add_imu(imu);
+  if(SEND_PITOT)
+    msg_handler.add_pitot(pitot);
+  if(SEND_GPS)
+    msg_handler.add_gps(gps);
+  if(SEND_ENV)
+    msg_handler.add_enviro(enviro);
+  if(SEND_BATT)
+    msg_handler.add_battery(batt);
+  if(SEND_SYSTEM)
+    msg_handler.add_config(system_config);
+  if(SEND_STATUS)
+    msg_handler.add_status(status_state);
+  if(SEND_SERVOS)
+    msg_handler.add_actuators(servos);
+  if(SEND_AIRDATA)
+    msg_handler.add_airdata(airdata);
+  if(SEND_CMDS)
+    msg_handler.add_cmds(commands);
+  if(SEND_DROPALGO)
+    msg_handler.add_drop(dropalgo);
   
   RawMessage_t raw_msg = msg_handler.build(ID::G1, ID::G2);
   char *buf = (char *) &raw_msg;
