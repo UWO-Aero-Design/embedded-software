efe271d62134a80d32d9468ce3d1566b579a2717
Jeff St. Jean <jeffrey@stjean.ca>
2019-11-08 14:13:25 -0500

    Consolidated into one file

--- western-aero-2019-2020/Rfm95w.h
+++ western-aero-2019-2020/Rfm95w.h
@@ -14,13 +14,49 @@ using namespace aero;
 using namespace aero::teensy35;
 using namespace aero::def;
 
+#define led 33 // temp
+
 class Rfm95w{
   public:
-    Rfm95w();
-    ~Rfm95w();
+    Rfm95w() {
+      rf95 = new RH_RF95(SPI0.CS, INTERRUPT_PIN);
+    };
+    ~Rfm95w() {
+      delete rf95;
+    };
+
+    /**
+     * @brief Initialize the radio
+     */
+    void init() {
+      pinMode(RESET_PIN, OUTPUT);
+      pinMode(led, OUTPUT); // temp
     
-    void init();
-    void send(char *msg);
+      // Manual radio reset
+      digitalWrite(RESET_PIN, LOW);
+      delay(10);
+      digitalWrite(RESET_PIN, HIGH);
+      delay(10);
+    
+      if(!rf95->init()) {
+        Serial.println("LoRa radio init failed");
+      }
+      else {
+        Serial.println("LoRa radio init OK!");
+      }
+    };
+
+    /**
+     * @brief Send data via radio
+     * 
+     * @param buf A (max 256 byte) buffer containing the message to be sent
+     */
+    void send(char *msg) {
+      digitalWrite(led, HIGH);    
+      rf95->send((char *)&msg, sizeof(msg));
+      rf95->waitPacketSent();
+      digitalWrite(led, LOW);
+    };
     
     String type = String("This is a radio");
 
