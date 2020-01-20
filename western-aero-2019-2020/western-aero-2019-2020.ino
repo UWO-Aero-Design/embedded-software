a368a0208e5a2aafcbc100575563444011e2d15d
Jeff St. Jean <jeffrey@stjean.ca>
2020-01-19 21:34:17 -0500

    Added dip selection. Issues passing to select method.

--- western-aero-2019-2020/western-aero-2019-2020.ino
+++ western-aero-2019-2020/western-aero-2019-2020.ino
@@ -3,24 +3,30 @@
 
 const uint8_t BUILTIN_LED = 13;
 const int DEFAULT_BAUD = 9600;
+const uint8_t DIP_SWITCHES[] = { 24, 25, 26, 27 };
 
 // will eventually be read from 4 pos dip switch
 #define SYSTEM_SELECTION SystemSelect::CompSystem_t
+uint8_t selection = 0;
 
 System *sys = NULL;
 
 void setup() {
-  Serial.print("Booting in ");
-  Serial.print(SystemSelect::get_description(SYSTEM_SELECTION));
-  Serial.println(" mode");
-  
-  // create the system specified by user input
-  sys = SystemSelect::select(SYSTEM_SELECTION);
-  sys->init();
-  
   if(!Serial) {
     Serial.begin(DEFAULT_BAUD);
   }
+  for(int i = 3; i >= 0; i--) {
+    pinMode(DIP_SWITCHES[i], INPUT);
+    selection = selection | (digitalRead(DIP_SWITCHES[i]) << i);
+  }
+  Serial.println(selection, BIN);
+  Serial.print("Booting in ");
+  Serial.print(SystemSelect::get_description(selection));
+  Serial.println(" mode");
+  
+  // create the system specified by user input
+  sys = SystemSelect::select(selection);
+  sys->init();
 }
 
 void loop() {
