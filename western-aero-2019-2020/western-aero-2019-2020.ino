9d5ad3a0843bb709fe765fa30d4a4e44735b24e6
Jeff St. Jean <jeffrey@stjean.ca>
2020-03-05 01:29:01 -0500

    Conditional compiling for ground station

--- western-aero-2019-2020/western-aero-2019-2020.ino
+++ western-aero-2019-2020/western-aero-2019-2020.ino
@@ -10,28 +10,34 @@ const uint8_t BUILTIN_LED = 13;
 const int DEFAULT_BAUD = 9600;
 const uint8_t DIP_SWITCHES[] = { 24, 25, 26, 27 };
 
-uint8_t system_selection = 0;
-// uint8_t system_selection = SystemSelect::GroundStation_t;
 System *sys = NULL;
+
+#ifdef GROUND_STATION
+uint8_t system_selection = SystemSelect::GroundStation_t;
+#endif
+#ifndef GROUND_STATION
+uint8_t system_selection = 0;
+#endif
 
 long last_update = 0;
 bool state = false;
 
 void setup() {
-  Serial.begin(DEFAULT_BAUD);
 
-//  // read dip switches
+  #ifndef GROUND_STATION
+  // read dip switches
   for(int i = 3; i >= 0; i--) {
     pinMode(DIP_SWITCHES[i], INPUT);
     system_selection = system_selection | (digitalRead(DIP_SWITCHES[i]) << i);
   }
+  #endif
+  sys = SystemSelect::select(system_selection);
   
   Serial.print("Booting in ");
   Serial.print(SystemSelect::get_description(system_selection));
   Serial.println(" mode\n");
 
   // create the system specified by user input
-  sys = SystemSelect::select(system_selection);
   if(sys->init()) {
     Serial.println("\nSystem successfully started.\n\n");
   }
@@ -43,15 +49,5 @@ void setup() {
 }
 
 void loop() {
-  if(state == HIGH && millis() - last_update >= 1) {
-    digitalWrite(23, LOW);
-    state = LOW;
-    last_update = millis();
-  }
-  if(state == LOW && millis() - last_update >= 500) {
-    digitalWrite(23, HIGH);
-    state = HIGH;
-    last_update = millis();
-  }
   sys->update();
 }
