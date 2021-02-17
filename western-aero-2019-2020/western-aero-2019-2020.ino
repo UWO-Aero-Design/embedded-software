3604437c4c4aaed923530df7a4aef8314d66dc0d
JOYLAPTOP\Joy Shah <joyshah99@gmail.com>
2021-02-17 17:28:06 -0500

    master i2c comms between main and nav teensy

--- western-aero-2019-2020/western-aero-2019-2020.ino
+++ western-aero-2019-2020/western-aero-2019-2020.ino
@@ -4,12 +4,16 @@
 
 #include "Arduino.h"
 #include "System.hpp"
+#include "Wire.h"
 
 const uint8_t BUILTIN_LED = 13;
 const int DEFAULT_BAUD = 9600;
 //First 3 switches used to select system type while last switch used to select flight stabilization
 const uint8_t DIP_SWITCHES[] = { 24, 25, 26, 27 };
 const uint8_t AUX_CHANNEL = 12;
+
+constexpr int NAV_ADDRESS = 0x58; //Need to define based off onboard systems PCB
+bool nav_initialized = false;      //Stores state if nav board is initalized or not
 
 //aux_value stores the PWM input from the aux channel
 volatile int aux_value = 0;
@@ -41,11 +45,6 @@ void setup() {
   //Read flight stabilization mode from dip switch (pin 27)
   pinMode(DIP_SWITCHES[3], INPUT);
   flight_stable = digitalRead(DIP_SWITCHES[3]);
-  if (flight_stable){
-    attachInterrupt(digitalPinToInterrupt(AUX_CHANNEL), rising, RISING);
-    //Initialize i2c comms with nav teensy here
-
-  }
   #endif
   sys = SystemSelect::select(system_selection);
   
@@ -64,6 +63,24 @@ void setup() {
   else {
     Serial.println("\nSystem started with errors.\n\n");
   }
+  
+  //Adjustments to system selection based on if flight_stabilization is enabled
+  if (flight_stable){
+    attachInterrupt(digitalPinToInterrupt(AUX_CHANNEL), rising, RISING);
+    
+    //Initialize i2c with Navigation Teensy
+    Wire.beginTransmission(NAV_ADDRESS);
+    int response = Wire.endTransmission();
+
+    if(response != 0){
+      nav_initialized = false;
+      Serial.println ("Comms not established with nav board");
+    }
+    else{
+      nav_initialized = true;
+      Serial.println ("Comms established with nav board");
+    }
+  }
 }
 
 void loop() {
@@ -80,6 +97,19 @@ void loop() {
   }
   //Update the system
   sys->update();
+
+  //Update nav_board with status of flight control
+  if (flight_stable){
+    Wire.beginTransmission(NAV_ADDRESS);
+    //If aux_value > 1750, switch is ON. If less than 1750, switch is OFF
+    if (aux_value>1750){
+      Wire.write(1);
+    }
+    else{
+      Wire.write(0);
+    }
+    Wire.endTransmission();
+  }
 }
 
 //ISR for rising edge saves the time in microseconds and attachs interrupt to falling edge
