f7f4e0e1b0f94cf97c1e1b819eae06a931f47dd3
Jarrod Hasnain <jhasnain@uwo.ca>
2021-11-16 16:52:29 -0500

    Updated Enviro.hpp
    Adjusted the code to be compatible with the new BMP280 sensor. Added the BMP280 library

--- western-aero-2019-2020/Enviro.hpp
+++ western-aero-2019-2020/Enviro.hpp
@@ -2,24 +2,28 @@
  * @brief All code relating to the environment sensor
  */
 
+/**
+ * @brief Updated from previous code for MPL3115A2 sensor to new BMP280 sensor
+ */
+
 #pragma once
 
 // Include for sensor interfaces
 #include "src/aero-cpp-lib/include/Sensors.hpp"
 
 #include "Wire.h"
-#include "src/MPL3115A2/MPL3115A2.h"
+#include "src/Adafruit_BMP280/Adafruit_BMP280.h"
 
 /**
- * @brief Class for a enviornment sensor using the MPL3115A2 chip
+ * @brief Class for a enviornment sensor using the BMP280 chip
  * @details It is an I2C sensor capable of measuring pressure, humidity and temperature
  */
-class Mpl3115a2EnviroSensor : public aero::sensor::EnviroSensor {
+class AdafruitBMP280EnviroSensor : public aero::sensor::EnviroSensor {
 public:
     /**
-     * @brief Construct a new MPL3115A2 Enviro Sensor object
+     * @brief Construct a new BMP280 Enviro Sensor object
      */
-    Mpl3115a2EnviroSensor(){}
+    AdafruitBMP280EnviroSensor(){}
 
     /**
      * @brief Initialize sensor object
@@ -33,9 +37,6 @@ public:
         m_data.temperature = 0;
         
         enviro.begin();
-        enviro.setModeAltimeter();
-        enviro.setOversampleRate(7);
-        enviro.enableEventFlags(); 
     
         m_initialized = true;
         return m_initialized;
@@ -54,9 +55,9 @@ public:
         }
 
 //         if(millis() - last_update >= UPDATE_DELTA) {
-          float temperature = enviro.readTemp();
+          float temperature = enviro.readTemperature();
           float pressure = enviro.readPressure();
-          float _altitude = enviro.readAltitude();
+          float _altitude = enviro.readAltitude(zro_alt_pressure);
           float agl = _altitude - zero_altitude;
           if(agl < -10) agl = -10;
           float biased_value = agl;
@@ -81,7 +82,7 @@ public:
   
       float accumulated_pressure = 0.0f;
       for(int i = 0; i < SAMPLES; i++) {
-        float alt = enviro.readAltitude();
+        float alt = enviro.readAltitude(zero_al_pressure);
         accumulated_pressure += alt;
         delay(512); // Delay for update rate
       }
@@ -104,10 +105,11 @@ private:
     bool m_initialized = false;
 
     // An environment sensor
-    MPL3115A2 enviro;
+    Adafruit_BMP280 enviro;
 
     float raw_altitude = 0;
     float zero_altitude = 0;
+    float zero_alt_pressure = enviro.readPressure();
     static constexpr const float ELEVATION_OFFSET = 0.0f;
 
     long last_update = 0;
