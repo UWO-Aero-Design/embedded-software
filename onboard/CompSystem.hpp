b3efd8810d25f0504265136a1ff27057dc1ebfed
Jeff St. Jean <jeffrey@stjean.ca>
2022-04-20 16:06:49 -0400

    added LED controller class

--- onboard/CompSystem.hpp
+++ onboard/CompSystem.hpp
@@ -4,6 +4,9 @@
 
 #pragma once
 
+#define BUFFER_SIZE 128
+#define TELEMTRY_PRINT_INTERVAL 500
+
 #include "System.hpp"
 #include "src/Message/pb_encode.h"
 #include "src/Message/pb_decode.h"
@@ -77,19 +80,8 @@ class CompSystem : public System {
 
       Serial.println("\n");
       
-      Serial.print("Calibrating Enviro...");
-      if(enviro.calibrate()) {
-        Serial.println("Done.");
-      }
-      else {
-        Serial.println("Error calibrating the enviro.");
-        is_success = false;
-      }
-
-//      Serial.print("Calibrating IMU...");
-//      imu.calibrate();
-//      Serial.println("Done.");
-return true;
+      return is_success;
+      
     }
 
     bool update() override {
@@ -106,17 +98,17 @@ return true;
 
       // ---- receive message if ready --- //
       if(radio.ready()) {
-        if(receive_message(&message_to_receive)) {
+        if(receive_message(&message_buffer, &message_bytes_received)) {
           
           // ---- reply with telemetry --- //
           Serial.print("Received message (Packet: ");
-          Serial.print(message_to_receive.packet_number);
+          Serial.print(message_buffer.packet_number);
           Serial.println("), replying with telemetry");
-          message_to_send = Message_init_zero;
-          if(imu_success) imu_data_to_msg(&message_to_send, &imu_data);
-          if(enviro_success) enviro_data_to_msg(&message_to_send, &enviro_data);
-          if(gps_success) gps_data_to_msg(&message_to_send, &gps_data);
-          send_telemetry(&message_to_send, &radio_data, packet_number++);
+          message_buffer = Message_init_zero;
+          if(imu_success) imu_data_to_msg(&message_buffer, &imu_data);
+          if(enviro_success) enviro_data_to_msg(&message_buffer, &enviro_data);
+          if(gps_success) gps_data_to_msg(&message_buffer, &gps_data);
+          send_telemetry(&message_buffer, &radio_data, packet_number++);
           
         }
         
@@ -125,8 +117,9 @@ return true;
       }
 
       // ---- print debug data --- //
-      if(millis() - last_print >= 500) {
+      if(millis() - last_print >= TELEMTRY_PRINT_INTERVAL) {
         // fill print buffer with formatted text
+        char print_buffer[BUFFER_SIZE];
         sprintf(print_buffer, "IMU [accel]: %-7.2f %-7.2f %-7.2f\tEnviro [A/T/P]: %-7.2f %-7.2f %-7.2f\tSats: %-i",
               imu_data.ax, imu_data.ay, imu_data.az,
               enviro_data.altitude, enviro_data.temperature, enviro_data.pressure , gps_data.satellites);
@@ -140,21 +133,15 @@ return true;
   protected:
 
   private:
-    long last_print = 0;
-    long last_led_update = 0;
-    long last_telemetry_send = 0;
-    uint32_t packet_number = 0;
-    uint8_t send_buffer[128];
-    Message message_to_receive;
-    Message message_to_send;
+    long last_print = 0; /*! The last time a message was printed to the serial monitor */
+    uint32_t packet_number = 0; /*! The last time a message was printed to the serial monitor */
+    uint8_t message_bytes_received = 0;
+    Message message_buffer;
     pb_ostream_t send_stream;
     pb_istream_t receive_stream;
     IMU imu_msg;
     Enviro enviro_msg;
     GPS gps_msg;
-  
-    // holds the nicely formatted sensor data string for printing
-    char print_buffer[256];
 
     // Structs for data
     aero::def::IMU_t imu_data;
@@ -172,7 +159,6 @@ return true;
 
     // Sensors
     ImuIcm20948 imu;
-    PhidgetPitotTube pitot {aero::teensy35::P14_PWM};
     AdafruitBMP280EnviroSensor enviro;
     Radio_Rfm95w radio;
     AdafruitGPS gps {&Serial2};
@@ -180,23 +166,16 @@ return true;
     
     bool gps_fix = false;
     uint8_t GPS_FIX_PIN = aero::teensy35::P16;
-    DropAlgo algo = DropAlgo(28.084217, -81.965614);
+//    DropAlgo algo = DropAlgo(28.084217, -81.965614);
 
-    // bitmasks for Commands_t struct of ParsedMessage_t
-    const uint8_t OPEN_DOORS_MASK      = 0b00000001;
-    const uint8_t CLOSE_DOORS_MASK     = 0b00000010;
-    const uint8_t GLIDER_DROP_MASK     = 0b00000100;
-    const uint8_t WATER_DROP_MASK      = 0b00001000;
-    const uint8_t HABITATS_DROP_MASK   = 0b00010000;
-
-    bool receive_message(Message *message) {
+    bool receive_message(Message *message, uint8_t *bytes_received) {
       bool message_received = false;
       uint8_t receive_buffer[radio.RECEIVE_BUFFER_SIZE];
-      uint8_t bytes_received = radio.RECEIVE_BUFFER_SIZE;
-      if(radio.receive(receive_buffer, &bytes_received)) {
-        message_received = true;
+      if(radio.receive(receive_buffer, bytes_received)) {
+        
         // init message
         *message = Message_init_zero;
+        message_received = true;
 
         // set callback function for each command in command list
         // pass reference to this class so that static callback has access to instance members
@@ -206,9 +185,11 @@ return true;
         // set up receive stream and decode
         receive_stream = pb_istream_from_buffer(receive_buffer, bytes_received);
         bool status = pb_decode(&receive_stream, Message_fields, message);
-        
+
+        // if unsuccessful
         if (!status) {
-          Serial.print("Error decoding message: "); Serial.println(PB_GET_ERROR(&receive_stream));
+          Serial.print("Error decoding message: ");
+          Serial.println(PB_GET_ERROR(&receive_stream));
         }
         else {
           // message successful decoded
@@ -222,6 +203,15 @@ return true;
       return message_received;
     }
 
+    /**
+     * @brief Prepares a header with for a message
+     *
+     * @param msg The message buffer
+     * @param recipient The intended recipient of the message
+     * @param packet_number The packet number to be loaded
+     * @param status The current status of the aircraft
+     * @param rssi The last RSSI of the radio
+     */
     void load_header(Message *msg, Message_Location recipient, uint32_t packet_number, Message_Status status, uint16_t rssi) {
       msg->sender = Message_Location::Message_Location_PLANE;
       msg->recipient = recipient;
@@ -231,6 +221,22 @@ return true;
       msg->rssi = rssi;
     }
 
+    /**
+     * @brief Callback function  wrapper to execute a member 
+     * @detailed Since the PB decode routine expects a free floating function and we want to pass
+     * a member function (which has an extra "this" argument at the end), this wrapper function will
+     * accept (vois **arg) the pointer to the instance with which you want to call a member function 
+     * for. The PB setup for this looks like:
+     * message.<repeated property>.funcs.decode = static_command_decode_callback;
+     * message.<repeated property>.arg = this; // "this" refers to the instance to have the member function called for
+     * Note: all passed in arguments are defined by NanoPB
+     *
+     * @param istream The Pb in stream
+     * @param field The field that is passed in
+     * @param arg The user defined arguments
+     */
+    // TODO: accept the decode routine and command to excute with any args as well so
+    // that this call back wrapper can be used for any member function
     static bool static_command_decode_callback(pb_istream_t *istream, const pb_field_t *field, void **arg) { 
       // arg passed in is reference to the calling class
       // cast so we can call member function after decoding
@@ -285,8 +291,9 @@ return true;
     }
 
     void send_telemetry(Message *message, aero::def::Radio_t *radio_data, uint32_t packet_number) {
+      uint8_t send_buffer[BUFFER_SIZE];
       load_header(message, Message_Location::Message_Location_GROUND_STATION, packet_number, Message_Status::Message_Status_READY, radio_data->rssi);
-      message_to_send.flight_stabilization = Message_FlightStabilization::Message_FlightStabilization_NONE;
+      message->flight_stabilization = Message_FlightStabilization::Message_FlightStabilization_NONE;
       
       send_stream = pb_ostream_from_buffer(send_buffer, sizeof(send_buffer));
       bool status = pb_encode(&send_stream, Message_fields, message);
