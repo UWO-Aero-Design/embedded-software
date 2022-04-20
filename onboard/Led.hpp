f597e515bc390cf2ed55a8a7b1254ea7fec7580b
Jeff St. Jean <jeffrey@stjean.ca>
2022-04-20 19:15:19 -0400

    created led controller

--- onboard/Led.hpp
+++ onboard/Led.hpp
@@ -25,3 +25,58 @@ class Animation {
     uint32_t pin;
     uint32_t enabled = true;
 };
+
+/**
+   @brief Class for controlling LEDs
+   @details allows for animating LEDs in an asynchronous manner
+*/
+class LedController {
+  public:
+
+    /**
+       @brief Construct a new Led controller object
+    */
+    LedController() {};
+
+    bool update() {
+      Animation *index = head;
+      uint32_t count = 0;
+      bool update_occured = false;
+
+      while(index != NULL) {
+        update_occured |= index->update();
+        index = index->next;
+      }
+
+      return update_occured;
+    }
+
+    bool attach(Animation *animation) {
+      if(head == NULL) {
+        head = animation;
+      }
+      else {
+        Animation *index = head;
+        if(index == animation) {
+          Serial.println("Error: Animation already exists");
+          return false;
+        }
+        
+        while(index->next != NULL) {
+          if(index == animation) {
+            Serial.println("Error: Animation already exists");
+            return false;
+          }
+          index = index->next;
+        }
+        index->next = animation;
+      }
+      animation_count++;
+      return true;
+    }
+
+  private:
+    uint32_t animation_count = 0;
+    Animation *head = NULL, *tail = NULL;
+    
+};
