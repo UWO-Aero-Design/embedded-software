2dcd5d43c3414516a7206e2180e495826b6527fe
Jeff St. Jean <jeffrey@stjean.ca>
2024-02-20 20:30:55 -0800

    add length prefix to onboard radio class

--- onboard/Radio_Rfm95w.hpp
+++ onboard/Radio_Rfm95w.hpp
@@ -6,6 +6,8 @@
 /**
    @brief Class for a radio using the RFM95W radio
    @details It is a LoRa radio capable of sending and receiving data over RF
+   @note all messages sent to/from the radio shall be prepended with a single byte
+         representing the length of the message (eg. { 5, _, _, _, _, _ })
 */
 class Radio_Rfm95w : public aero::sensor::Radio {
   public:
@@ -20,6 +22,7 @@ class Radio_Rfm95w : public aero::sensor::Radio {
       m_data.rssi = 0;
       m_data.frequencyError = 0;
       m_data.snr = 0;
+      bytesReceived = 0;
     }
 
     /**
@@ -64,11 +67,59 @@ class Radio_Rfm95w : public aero::sensor::Radio {
     }
 
     bool receive(uint8_t* buf, uint8_t* len) override {
-      return radio->recv(buf, len);
+      uint8_t bufLen = RH_RF95_MAX_MESSAGE_LEN-bytesReceived;
+      if(!radio->recv(&recvBuffer[bytesReceived], &bufLen)) {
+        // recv failed
+        return false;
+      }
+      else if(bufLen == 0) {
+        // empty message received
+        return false;
+      }
+
+      if(bytesReceived == 0) {
+        // this is the first recv() called for this message
+        msgLength = recvBuffer[0];
+
+        if(msgLength == 0 || msgLength > RH_RF95_MAX_MESSAGE_LEN) {
+          return false;
+        }
+      }
+
+      // increment bytes received by the number of bytes we just received
+      bytesReceived += bufLen;
+
+      // if we received the right number of bytes
+      if(bytesReceived-1 >= msgLength) {
+        // copy the message into the caller's buffer and return success
+        for(uint8_t i = 0; i < msgLength; i++) {
+          buf[i+1] = recvBuffer[i];
+        }
+        bytesReceived = 0;
+        return true;
+      }
+
+      // partial message - need main script to call receive() again
+      return false;
     }
 
     bool send(uint8_t* buf, uint8_t len) override {
-      return radio->send(buf, len);
+      // verify we have enough room in our local buffer
+      if(len > RH_RF95_MAX_MESSAGE_LEN-1) {
+        Serial.println("Buffer overflow");
+        return false;
+      }
+
+      // put the length in the first byte
+      sendBuffer[0] = len;
+
+      // copy the rest of the message into the send buffer
+      for(uint8_t i = 0; i < len; i++) {
+        sendBuffer[i+1] = buf[i];
+      }
+
+      // send the length and message
+      return radio->send(sendBuffer, len+1);
     }
 
   protected:
@@ -78,5 +129,10 @@ class Radio_Rfm95w : public aero::sensor::Radio {
   private:
     static constexpr float RADIO_FREQ = 433 ;
     static constexpr int RADIO_POWER = 23; // Max power
+
+    uint8_t sendBuffer[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t recvBuffer[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t msgLength = 0;
+    uint8_t bytesReceived = 0;
 
 };
